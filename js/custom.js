602da6824e6775ec615c6a2b10da1de623eaa4dd
QuynhAnhNgo <162398334+QuynhAnhNgo@users.noreply.github.com>
2024-03-17 21:11:57 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+$(document).ready(function(){
+	$(".btn-menu").click(function(){
+		$(".menu").slideToggle()
+	})
+})
+$('#banner').owlCarousel({
+    loop:true,
+    margin:10,
+   /* nav:true,*/
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+    autoplayTimeout:5000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
